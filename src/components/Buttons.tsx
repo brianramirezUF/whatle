5db5255093c9bab379d59d25afb964ddb66d5cf2
Brian Ramirez <brian.ramirez@ufl.edu>
2025-02-06 12:24:50 -0500

    Basic navigation + use client component

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useRouter } from "next/navigation";
+
+function RedirectButton(props: { url: string, text: string}){
+    const { url, text } = props;
+    const router = useRouter();
+
+    return(
+        <button onClick={()=>router.push(url)}>{text}</button>
+    )
+}
+
+export{RedirectButton}
