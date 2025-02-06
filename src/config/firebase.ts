f01a59ea70017f312a9cb56da81fce23bc657eb3
Brian Ramirez <brian.ramirez@ufl.edu>
2025-02-06 01:26:59 -0500

    fire base install & config

---
+++ src/config/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+
+if(!process.env.firebase){
+    throw new Error("Environment variables missing")
+}
+
+const env = JSON.parse(process.env.firebase);
+
+const firebaseConfig = {
+    apiKey: env.apiKey,
+    authDomain: env.authDomain,
+    projectId: env.projectId,
+    storageBucket: env.storageBucket,
+    messageSenderId: env.messageSenderId,
+    appId: env.appId,
+    measurementId: env.measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
