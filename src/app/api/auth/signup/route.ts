e95c638c9fca3eab35f95544e411d3ace89a26f0
Brian Ramirez <100593894+brianramirezUF@users.noreply.github.com>
2025-02-09 02:56:23 -0500

    Testing imgur api redirect on preview (#4)
    * basic user signup/login
    
    * preview test
    
    * Revert "preview test"
    
    This reverts commit 7782ce69b1140ab50ef7f4f5df02d44264846f4b.
    
    * Imgur api redirect
    
    * bug fix

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -3,6 +3,15 @@ import { auth, db } from "@/config/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 
+const envUrl = 
+process.env.NODE_ENV === "production" ? "https://whatle.com/api/auth/imgur-callback":
+process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"? "https://preview.whatle.com/api/auth/imgur-callback": 
+"http://localhost:3000/api/auth/imgur-callback";
+
+console.log(envUrl);
+
+const IMGUR_CLIENT_ID = process.env.imgur_clientId;
+const IMGUR_REDIRECT_URI = encodeURIComponent(envUrl);
 export async function POST(req: Request) {
     try {
         const { email, password } = await req.json();
@@ -18,7 +27,9 @@ export async function POST(req: Request) {
             email: user.email
         });
 
-        return NextResponse.json({ user: user }, { status: 201 });
+        const authUrl = `https://api.imgur.com/oauth2/authorize?client_id=${IMGUR_CLIENT_ID}&response_type=token&redirect_uri=${IMGUR_REDIRECT_URI}&state=${encodeURIComponent(user.uid)}`;
+
+        return NextResponse.json({ user: user, imgurAuthUrl: authUrl }, { status: 201 });
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
