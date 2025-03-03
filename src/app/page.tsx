42b52d4d91956a653cd399ee3996255e7f24a444
Brielle Stokes <briellestokes@ufl.edu>
2025-03-02 23:37:31 -0500

    navbar, logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { RedirectButton } from '@/components/Buttons'
+import React from 'react';
+import { NavBar } from '@/components/NavBar'
 
 export default function Home() {
   return (
     <>
+      <NavBar></NavBar>
       <RedirectButton url="/create" text="create"></RedirectButton>
+
     </>
   );
 }
