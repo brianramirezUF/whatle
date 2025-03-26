beea2231aa48a189ebb55653bcee3192dbf8c0d9
EmelyChhu <emelychhu@gmail.com>
2025-03-26 14:21:27 -0400

    Add test images to carousel and navigate create button to /game

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,6 +7,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import { Button } from "@/components/ui/button"
+import { RedirectButton } from '@/components/Buttons'
 import "./styles.css";
 
 type Game = {
@@ -20,18 +21,22 @@ export default function Home(){
     {
       id: "4",
       name: "MLBdle",
+      icon: "https://i.imgur.com/B7wb5i6_d.webp?maxwidth=520&shape=thumb&fidelity=high",
     },
     {
       id: "2",
       name: "Pokedle",
+      icon: "https://i.imgur.com/5ValAxb.jpeg",
     },
     {
       id: "3",
       name: "NBAdle",
+      icon: "https://i.imgur.com/PV54ZYq_d.webp?maxwidth=520&shape=thumb&fidelity=high",
     },
     {
       id: "1",
       name: "Marvelde",
+      icon: "https://i.imgur.com/YZwdjey.jpeg",
     },
   ];
   
@@ -48,9 +53,14 @@ export default function Home(){
           {featuredGames.map((game, index) => (
             <CarouselItem key={index} className="basis-1/3">
               <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-3xl font-semibold">{game.name}</span>
+                <Card
+                  className="card"
+                  style={game.icon ? {
+                    backgroundImage: `url(${game.icon})`,
+                  } : {}}
+                >
+                  <CardContent className="card-content flex aspect-square items-center justify-center p-6">
+                    <span className="text-3xl font-semibold text">{game.name}</span>
                   </CardContent>
                 </Card>
               </div>
@@ -63,7 +73,7 @@ export default function Home(){
         <h1 className="subtitle text-center font-medium">
           Create Your Own Game:
         </h1>
-        <Button className="button w-[100px]">Create</Button>
+        <RedirectButton url="/game" text="Create" className="button  w-[125px] h-[40px]"/>
       </div>
   )
 }
