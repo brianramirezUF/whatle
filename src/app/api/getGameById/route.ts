34a78e1eb3b0a75f185dbe609f4d33a3d3fd91e9
Brielle Stokes <briellestokes@ufl.edu>
2025-04-01 19:13:15 -0400

    getgameby idroute....

---
+++ src/app/api/getGameById/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { db } from "@/config/firebase";
+import { doc, getDoc } from 'firebase/firestore';
+
+// GET /api/getGameById?id=<gameId>
+export async function GET(req: Request)  {
+    const { searchParams } = new URL(req.url);
+    const gameId = searchParams.get("id");
+
+    if (!gameId) {
+        return NextResponse.json({ error: "Missing game ID!" }, { status: 400 });
+    }
+
+    try {
+        const gamePointer = doc(db, "games", gameId);
+        const gameSnapshot = await getDoc(gamePointer);
+
+        if (!gameSnapshot.exists()) {
+            return NextResponse.json({ error: "Game not found" }, { status: 404 });
+        }
+
+        const gameData = gameSnapshot.data();
+        return NextResponse.json({ id: gameSnapshot.id, ...gameData }, { status: 200 });
+    } 
+    
+    catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
+
+
