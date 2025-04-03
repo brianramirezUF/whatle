a74419c1bb9fdae546124c9c167f892db86764b0
EmelyChhu <emelychhu@gmail.com>
2025-04-03 14:54:49 -0400

    Update className RedirectButton prop to optional

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useRouter } from "next/navigation";
 
-function RedirectButton(props: { url: string, text: string, className: string}){
+function RedirectButton(props: { url: string, text: string, className?: string}){
     const { url, text, className } = props;
     const router = useRouter();
 
