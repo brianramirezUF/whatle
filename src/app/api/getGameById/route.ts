868f8165f2ccd7c98d9e4dc7ac2762d90aa13469
EmelyChhu <emelychhu@gmail.com>
2025-04-08 13:17:11 -0400

    Update getPopularGames endpoint to only return basic information

--- src/app/api/getGameById/route.ts
+++ src/app/api/getGameById/route.ts
@@ -23,7 +23,7 @@ export async function GET(req: Request)  {
         }
 
         const gameData = gameSnapshot.data();
-        return NextResponse.json({ id: gameSnapshot.id, ...gameData }, { status: 200 });
+        return NextResponse.json({ id: gameSnapshot.id, name: gameData.name, icon: gameData.icon }, { status: 200 });
     } 
     
     catch (error: any) {
