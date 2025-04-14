e424e34e1ac39e49c8c5d1d00a6966401b565deb
ahhhh W <noahwhelden@gmail.com>
2025-04-14 16:08:51 -0400

    Move guess logic to server

--- src/app/play/components.tsx
+++ src/app/play/components.tsx
@@ -1,7 +1,8 @@
 import { Icons } from '@/components/icons'
-import { useState } from 'react';
-import { Guess, AttributeType, AnswerType, comparisons, GuessStatus } from '../create/attributes';
+import { useEffect, useState } from 'react';
+import { Guess, AttributeType, AnswerType } from '../create/attributes';
 import { Card, CardContent } from "@/components/ui/card";
+import { GuessStatus } from '@/lib/guessComparison';
 import React from 'react';
 import "./styles.css";
 import { getAuth } from "firebase/auth";
@@ -31,33 +32,35 @@ const Games = [
   },
 ];
 
+type GuessResult = {
+  guess: string;
+  status: GuessStatus;
+  details?: string;
+};
+
+type GuessResultContainer = {
+  result: GuessResult[],
+  name: string
+};
+
 export interface GameProps {
   answers: Record<string, AnswerType>,
   attributes: AttributeType[],
-  name: string
+  name: string,
+  gameId?: string
 }
 
-
-
-const Game: React.FC<GameProps> = ({ answers, attributes, name }) => {
-  const [guesses, setGuesses] = useState<string[]>([]);
+const Game: React.FC<GameProps> = ({ answers, attributes, name, gameId }) => {
+  const [guesses, setGuesses] = useState<GuessResultContainer[]>([]);
   const [curGuess, setCurGuess] = useState<string>('');
-  const [correctAnswer, setCorrectAnswer] = useState<AnswerType | null>(null);
   const [filteredAnswers, setFilteredAnswers] = useState<AnswerType[]>(Object.values(answers));
   const [showDropdown, setShowDropdown] = useState(false);
-  let won = false;
+  const [won, setWon] = useState(false);
   const [confettiBursts, setConfettiBursts] = useState<{ id: number; x: number; y: number }[]>([]);
 
   if (!Object.keys(answers).length) {
     return <h1>No answers.</h1>
   }
-
-  const getRandomAnswer = (): AnswerType => {
-    const keys = Object.keys(answers);
-    const answer = answers[keys[Math.floor(Math.random() * keys.length)]];
-
-    return answer;
-  };
 
   const handleInputChange = (value: string) => {
     setCurGuess(value);
@@ -81,24 +84,29 @@ const Game: React.FC<GameProps> = ({ answers, attributes, name }) => {
     setShowDropdown(false);
   };
 
-  // Reset stateful values
-  const newGame = () => {
+  const resetGame = () => {
+    setWon(false);
     setGuesses([]);
     setCurGuess('');
-
-    setCorrectAnswer(getRandomAnswer());
-    console.log(correctAnswer);
   };
 
   // Update guesses array (each guess is an index of an answer from the 'answers' array)
   const makeGuess = async (guess: string) => {
-    if (!guess || !correctAnswer) return;
+    if (!guess || won) return;
 
-    setGuesses((prev) => [...prev, guess]);
+    const res = await fetch("/api/checkGuess", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ gameId, guess }), // make sure `gameId` is available in this scope
+    });
 
-    const isWin = guess === correctAnswer.name;
-    won = isWin;
+    if (!res.ok) {
+      throw new Error('Failed to make guess.');
+    }
 
+    const { results, isWin } = await res.json();
+    setWon(isWin);
+    setGuesses((prev) => [...prev, { result: results, name: guess }]);
 
     const auth = getAuth();
     const currentUser = auth.currentUser;
@@ -110,7 +118,6 @@ const Game: React.FC<GameProps> = ({ answers, attributes, name }) => {
     }
 
     const userId = currentUser.uid;
-    const gameId = "4"; // Or whatever game you're testing
     const timeTaken = Math.floor(Math.random() * 100) + 1;
 
     const payload = {
@@ -152,36 +159,36 @@ const Game: React.FC<GameProps> = ({ answers, attributes, name }) => {
     };
 
     if (isWin) {
-      setConfettiBursts([]);
-      launchBursts();
+      // Wait for full answer to render before doing win animation
+      setTimeout(() => {
+        setConfettiBursts([]);
+        launchBursts();
+      }, attributes.length * 400);
     }
 
-
-    try {
-      const res = await fetch("/api/updateUserHistory", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      });
-
-      const data = await res.json();
-
-      if (!res.ok) {
-        // console.error("❌ API Error Response:", data);
-        // alert("❌ Failed to update Firebase: " + (data.error || "Unknown error"));
-        // return;
+    // Only update history when the user wins
+    if (isWin) {
+      try {
+        const res = await fetch("/api/updateUserHistory", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        });
+  
+        if (!res.ok) {
+          // console.error("❌ API Error Response:", data);
+          // alert("❌ Failed to update Firebase: " + (data.error || "Unknown error"));
+          // return;
+        }
+  
+      } catch (err: any) {
+        console.error("❌ Network/Code Error:", err.message);
+        //alert("❌ Error updating Firebase: " + err.message);
       }
-
-    } catch (err: any) {
-      console.error("❌ Network/Code Error:", err.message);
-      //alert("❌ Error updating Firebase: " + err.message);
     }
   };
-
-
-
 
   const renderHeaders = () => (
     <>
@@ -195,10 +202,7 @@ const Game: React.FC<GameProps> = ({ answers, attributes, name }) => {
   );
 
   const renderRows = () => {
-    return guesses.map((guessName, rowIndex) => {
-      const guessedAnswer = answers[guessName];
-      if (!guessedAnswer || !correctAnswer) return null;
-
+    return guesses.map((guess, rowIndex) => {
       return (
         <React.Fragment key={`row-${rowIndex}`}>
           <div>
@@ -207,26 +211,26 @@ const Game: React.FC<GameProps> = ({ answers, attributes, name }) => {
                 backgroundImage: `url(${Games[3].icon})`,
               } : {}}
             >
-              {/* <CardContent className="text-center">{guessName}</CardContent> */}
               <CardContent className="card-content flex aspect-square items-center justify-center truncate">
-                <span className="text-2xl text">{guessName}</span>
+                <span className="text-2xl text">{guess.name}</span>
               </CardContent>
             </Card>
           </div>
-          {attributes.map((attr: AttributeType, colIndex: number) => (
+          {guess.result.map((cur, colIndex: number) => (
             <Guess
               key={`cell-${rowIndex}-${colIndex}`}
               colIndex={colIndex}
               rowIndex={rowIndex}
-              guess={guessedAnswer.attributes[attr.name] || 'Value'}
-              answer={correctAnswer.attributes[attr.name] || 'Value'}
-              type={attr.type}
+              guess={cur.guess || 'Value'}
+              status={cur.status}
+              details={cur.details}
             />
           ))}
         </React.Fragment>
       );
     });
   };
+
   return (
     <div className="flex flex-col justify-center items-center p-4">
       {confettiBursts.map((burst) => (
@@ -234,8 +238,9 @@ const Game: React.FC<GameProps> = ({ answers, attributes, name }) => {
       ))}
       <div className="max-w-6xl w-full flex flex-col bg-white rounded-lg p-6 mb-4 justify-center items-center">
         <h2 className="text-xl mb-4 font-bold text-center">{name}</h2>
-        <div className="flex items-center gap-2 w-full max-w-md">
-          {!won &&
+
+        {!won &&
+          <div className="flex items-center gap-2 w-full max-w-md">
             <div className="relative w-full">
               <input
                 type="text"
@@ -257,25 +262,23 @@ const Game: React.FC<GameProps> = ({ answers, attributes, name }) => {
                   ))}
                 </div>
               )}
-            </div>}
-          <button
-            onClick={() => makeGuess(curGuess)}
-            className="px-4 py-2 bg-red-500 text-white rounded">
-            Guess
-          </button>
+            </div>
+            <button
+              onClick={() => makeGuess(curGuess)}
+              className="px-4 py-2 bg-red-500 text-white rounded">
+              Guess
+            </button>
+          </div>}
+        <div
+          className="grid gap-2 mt-10"
+          style={{ gridTemplateColumns: `repeat(${attributes.length + 1}, minmax(100px, 300px))` }}
+        >
+          {renderHeaders()}
+          {renderRows()}
         </div>
-        {correctAnswer && (
-          <div
-            className="grid gap-2 mt-10"
-            style={{ gridTemplateColumns: `repeat(${attributes.length + 1}, minmax(100px, 300px))` }}
-          >
-            {renderHeaders()}
-            {renderRows()}
-          </div>
-        )}
 
-        <button onClick={newGame} className="mt-4 px-4 py-2 bg-red-500 text-white rounded">
-          New Game
+        <button onClick={resetGame} className="mt-4 px-4 py-2 bg-red-500 text-white rounded">
+          Reset
         </button>
       </div>
     </div>
