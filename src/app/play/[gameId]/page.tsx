cdd5082b6ec7b5efc99e33470dbf7b044aef122f
ahhhh W <noahwhelden@gmail.com>
2025-04-14 16:08:39 -0400

    Update page.tsx

--- src/app/play/[gameId]/page.tsx
+++ src/app/play/[gameId]/page.tsx
@@ -6,52 +6,44 @@ import { AttributeType, AnswerType } from '../../create/attributes';
 import { Game, GameProps } from '../components';
 import { JsonParser } from '@/components/JsonParser';
 import { Card, CardContent } from "@/components/ui/card";
+import { useParams } from 'next/navigation';
 import React from 'react';
-import { useParams } from "next/navigation";
 
 export default function PlayGame() {
-    const [attributes, setAttributes] = useState<AttributeType[]>([]);
-    const [answers, setAnswers] = useState<Record<string, AnswerType>>({});
-    const [gameName, setGameName] = useState<string>('');
-
+    const [gameData, setGameData] = useState<GameProps | null>();
     const params = useParams();
-    const gameId = params.gameId;
+    const gameId = params.gameId as string;
 
     // call endpoint to receive game information
-      useEffect(() => {
-        fetch(`/api/getGameById?id=${gameId}`)
-          .then((res) => res.json())
-          .then((game) => {
-            if (game.attributes) {
-              setAttributes(game.attributes);
-            }
-
-            if (game.answers) {
-              setAnswers(game.answers);
-            }
-
-            if (game.name) {
-              setGameName(game.name);
-            }
-            console.log("Fetched game:", game);
-          })
-          .catch((err) => {
-            console.error("Failed to fetch game:", err);
-          });
-      }, []);
-
-    const handleJSON = (data: GameProps) => {
-        setAnswers(data.answers);
-        setAttributes(data.attributes);
-        setGameName(data.name);
-    }
+    useEffect(() => {
+      fetch(`/api/getGameById?id=${gameId}`)
+        .then((res) => res.json())
+        .then((game) => {
+          setGameData(game);
+        })
+        .catch((err) => {
+          console.error("Failed to fetch game:", err);
+        });
+    }, [gameId]);
+     
+    const doTestUpdate = async () => {
+      await fetch('/api/testUpdate', {
+        headers: {
+          'x-cron-secret': `${process.env.NEXT_PUBLIC_CRON_SECRET}`
+        }
+      }); 
+    };
 
     const content = (
         <div>
-            <JsonParser onParse={handleJSON}/>
-            <h1 className='font-bold'>{gameName}</h1>
-            <Game answers={answers} attributes={attributes} name={gameName}/>
+            {gameData ? <Game {...gameData} gameId={gameId} /> : 'Failed to load game.'}
+            <div>
+              <button onClick={doTestUpdate}>
+                Cron Update
+              </button>
+            </div>
         </div>
+        
     );
 
     return content;
