a0c4a245890c29bdf0b202095ba5d4956b8e3549
Brian Ramirez <brian.ramirez@ufl.edu>
2025-04-17 12:07:59 -0400

    please fix icon

--- src/app/create/[gameId]/page.tsx
+++ src/app/create/[gameId]/page.tsx
@@ -106,13 +106,7 @@ export default function CreateGame() {
 
                 if (data.icon) {
                     console.log("Setting icon to", data.icon);
-                    if(imageDropRef.current){
-                        console.log("imageDropRef.current is NOT null");
-                        imageDropRef.current.setImageLink(data.icon);
-                    }
-                    else {
-                        console.error("imageDropRef.current is null");
-                    }
+                    setImageLink(data.icon);
                 }
 
                 if (data.tag) {
@@ -129,6 +123,14 @@ export default function CreateGame() {
 
         loadGame();
     }, [gameId]);
+
+    useEffect(() => {
+        if (imageDropRef.current && imageLink) {
+            imageDropRef.current.setImageLink(imageLink);
+        } else {
+            console.error("imageDropRef.current is null");
+        }
+    }, [imageDropRef, imageLink]);
 
     const uploadGame = async () => {
         if (!currentUser) return;
